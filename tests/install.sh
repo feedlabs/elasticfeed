32ae4db3f0f6dd3e9d05deb769e913808e35baab
Chris Stasiak <me@krzysztof-stasiak.com>
2014-08-16 10:50:55 +0200

    Fixed installer

--- tests/install.sh
+++ tests/install.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+GOPATH_ENV=$1
+SOURCE_DIR=$2
+
+export GOPATH=$GOPATH_ENV
+
+# create symlink of project into GOPATH
+FEEDLABS_PACKAGE_DIR=$GOPATH/src/github.com/feedlabs
+FEEDLABS_FEEDIFY_PATH=$FEEDLABS_PACKAGE_DIR/feedify
+
+if [ ! -e $FEEDLABS_FEEDIFY_PATH ]; then
+  mkdir -p $FEEDLABS_PACKAGE_DIR
+  ln -s $SOURCE_DIR/.. $FEEDLABS_FEEDIFY_PATH
+fi
+
+# install dependencies
 # go get github.com/bitly/go-nsq
