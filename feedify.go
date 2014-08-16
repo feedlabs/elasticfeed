cbd571d5c61aa8477b27f7918282984400fdeaeb
Chris Stasiak <me@krzysztof-stasiak.com>
2014-08-16 02:00:42 +0200

    Cleanup config load

--- feedify.go
+++ feedify.go
@@ -1,21 +1,11 @@
 package feedify
 
 import (
-	"flag"
 	"github.com/astaxie/beego"
 	_ "github.com/feedlabs/feedify/api"
 )
 
-func init() {
-	const (
-		defaultPort = 9090
-		usage       = "set port"
-	)
-	flag.IntVar(&beego.HttpPort, "port", defaultPort, usage)
-}
-
 func Run(port int) {
-	flag.Parse()
 	beego.HttpPort = port
 	beego.Run()
 }
