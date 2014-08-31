46d599761bafa6e177f79573ec7d6c686106ac46
Chris Stasiak <me@krzysztof-stasiak.com>
2014-08-31 11:16:46 +0200

    Moved config load to main package

--- tests/feedify.go
+++ tests/feedify.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"flag"
-	"strconv"
 	"github.com/feedlabs/feedify"
-	"github.com/astaxie/beego/config"
-
-	_ "github.com/feedlabs/feedify/lib/feedify/stream/adapter/message"
 )
 
-var configPath string
-var httpPort int
-
-func InitConfig() (config.ConfigContainer) {
-	const (
-		defaultConfigPath 	= "conf/app.conf"
-		defaultHttpPort		= 8080
-	)
-	flag.StringVar(&configPath, "config", defaultConfigPath, "set config file path")
-	flag.IntVar(&httpPort, "port", defaultHttpPort, "set http port")
-	flag.Parse()
-
-	CFPConfig, _ := config.NewConfig("ini", configPath)
-
-	return CFPConfig
-}
-
 func main() {
-	conf := InitConfig()
-	port, err := strconv.Atoi(conf.String("feedify::port"))
-
-	if (err == nil) {
-		httpPort = port
-	}
-
-	fmt.Printf("Starting app '%s' on port '%d'\n", conf.String("appname"), httpPort)
-
-	feedify.Run(httpPort)
+	feedify.Run()
 }
