b7deb9b22afb3cf020b71024e8f257e4513eee40
Chris Stasiak <me@krzysztof-stasiak.com>
2014-09-05 13:45:50 +0200

    Reorganized db package to service manager packages

--- lib/feedify/graph/adapter/cayley.go
+++ lib/feedify/graph/adapter/cayley.go
@@ -1,1 +1,18 @@
 package adapter
+
+import (
+	"log"
+)
+
+type CayleyAdapter struct {
+	host	string
+	port	string
+}
+
+func (m CayleyAdapter) Connect() {
+	log.Printf("%T connected", m)
+}
+
+func NewCayleyAdapter() *CayleyAdapter {
+	return &CayleyAdapter{}
+}
