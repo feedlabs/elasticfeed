7c493a3f2884ff4ff02086a2e922a34755c66f9d
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-07 17:08:30 +0100

    Updated feed controller and resource

--- resources/feed.go
+++ resources/feed.go
@@ -7,11 +7,15 @@ import (
 	"github.com/feedlabs/feedify/graph"
 )
 
-func init() {
-	Feeds = make(map[string]*Feed)
+func (this *Feed) AddEntry(entry Entry) (EntryId string, err error) {
+	return AddEntry(entry, this.Id, this.ApplicationId)
 }
 
-func GetFeedList() []*Feed {
+func (this *Feed) GetEntryList() (entries []*Entry, err error) {
+	return GetEntryList(this.Id, this.ApplicationId)
+}
+
+func GetFeedList(appId string) (feedList []*Feed, err error) {
 	nodes, err := storage.FindNodesByLabel(RESOURCE_FEED_LABEL)
 	if err != nil {
 		nodes = nil
@@ -24,14 +28,14 @@ func GetFeedList() []*Feed {
 		id := strconv.Itoa(node.Id)
 		rels, _ := storage.RelationshipsNode(node.Id, "contains")
 
-		feed := &Feed{id , data, len(rels)}
+		feed := &Feed{id , appId, data, len(rels)}
 		feeds = append(feeds, feed)
 	}
 
-	return feeds
+	return feeds, err
 }
 
-func GetFeed(id string) (feed *Feed, err error) {
+func GetFeed(id string, applicationId string) (feed *Feed, err error) {
 	_id, err := strconv.Atoi(id)
 	node, err := storage.Node(_id)
 
@@ -42,17 +46,32 @@ func GetFeed(id string) (feed *Feed, err error) {
 	if node != nil && contains(node.Labels, RESOURCE_FEED_LABEL) {
 		data := node.Data["data"].(string)
 		rels, _ := storage.RelationshipsNode(node.Id, "contains")
-		return &Feed{strconv.Itoa(node.Id), data, len(rels)}, nil
+		return &Feed{strconv.Itoa(node.Id), applicationId, data, len(rels)}, nil
 	}
 
 	return nil, errors.New("FeedId not exist")
 }
 
-func AddFeed(feed Feed) (id string, err error) {
-	properties := graph.Props{"data": feed.Data}
+func AddFeed(feed Feed, applicationId string) (id string, err error) {
+	// get feed
+	app, err := GetApplication(applicationId)
+	if err != nil {
+		return "0", err
+	}
+
+	// add feed
+	properties := graph.Props{"applicationId": applicationId, "data": feed.Data}
 	_feed, err := storage.NewNode(properties, RESOURCE_FEED_LABEL)
 
 	if err != nil {
+		return "0", err
+	}
+
+	// create relation
+	_appId, _ := strconv.Atoi(app.Id)
+	rel, err := storage.RelateNodes(_appId, _feed.Id, "contains", nil)
+
+	if err != nil || rel.Type == "" {
 		return "0", err
 	}
 
@@ -69,4 +88,8 @@ func UpdateFeed(id string, data string) (err error) {
 func DeleteFeed(id string) (error) {
 	_id, _ := strconv.Atoi(id)
 	return storage.DeleteNode(_id)
+}
+
+func init() {
+	Feeds = make(map[string]*Feed)
 }
