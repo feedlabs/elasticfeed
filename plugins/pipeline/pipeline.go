50bfd8c2ec2784f510ef8492a3aaab80b600b994
Chris Stasiak <me@krzysztof-stasiak.com>
2015-04-29 00:18:53 +0200

    Removed plugins dir

--- plugins/pipeline/pipeline.go
+++ plugins/pipeline/pipeline.go
@@ -1,5 +1,20 @@
 package pipeline
 
+import (
+	"time"
+	"math/rand"
+)
+
+func RandomAnimator(data interface {}) interface {} {
+
+	// PIPE DELAY SIMULATION
+
+	amt := time.Duration(rand.Intn(200))
+	time.Sleep(amt * time.Millisecond)
+
+	return data
+}
+
 func Filter(data interface{}) interface{} {
 	// should call plugins of type PIPELINE
 	return RandomAnimator(data)
--- plugins/sensor/sensors.go
+++
@@ -1,5 +0,0 @@
-package sensor
-
-func Weather() {}
-func StockIndices() {}
-func SunActivity() {}
