940180fc0f992647d22828c49732e3887940c8d7
Chris Stasiak <me@krzysztof-stasiak.com>
2015-06-06 17:43:40 +0200

    Added concept of REST client and Scraper for plugins

---
+++ common/rest/client.go
@@ -0,0 +1,13 @@
+package rest
+
+/*
+  SHOULD ALLOW FOR PLUGINS TO HANDLE RESTFUL APIs
+
+  e.g
+
+  - photo API by imagga.com
+  - weather API by worldweatheronline.com
+  - space weather by nasa.org
+  - crawling API by import.io
+  - prediction API by prediction.io
+*/
---
+++ common/scraper/scraper.go
@@ -0,0 +1,10 @@
+package scraper
+
+/*
+  SHOULD ALLOW FOR EASY SCRAPPING OF WEBSITEs
+
+  e.g.
+  - biorythm
+  - horoscope
+  - event in location/city
+*/
